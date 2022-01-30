5292192db6eca5467c201d8cd15e76001461d5f3
sbx_user1051 <sbx_user1051@169.254.3.253>
2022-01-30 20:29:52 +0000

    Sun Jan 30 20:29:52 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,21 +1,28 @@
 from collections import Counter
 
 PRICE_TABLE = {"A": 50, "B": 30, "C": 20, "D": 15}
+SPECIAL_OFFERS = {"A": (3, 130), "B": (2, 45)}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     for item in skus:
-        if item not in "ABCD":
+        if item not in PRICE_TABLE:
             return -1
 
     total = 0
 
     skus_counter = Counter(skus)
     for item, count in skus_counter.items():
-        if item == "A":
-            total += count // 3 * 130 + count % 3 * 50
+        if item in SPECIAL_OFFERS:
+            total += (
+                count // SPECIAL_OFFERS[item][0] * SPECIAL_OFFERS[item][1]
+                + count % SPECIAL_OFFERS[item][0] * PRICE_TABLE[item]
+            )
+        else:
+            total += count * PRICE_TABLE[item]
 
     return total
+
 
 
