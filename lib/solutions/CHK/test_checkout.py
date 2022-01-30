d47d9164346622bec689e8439777d04389dc01f3
sbx_user1051 <sbx_user1051@169.254.3.253>
2022-01-30 20:20:52 +0000

    Sun Jan 30 20:20:52 UTC 2022

--- lib/solutions/CHK/test_checkout.py
+++ lib/solutions/CHK/test_checkout.py
@@ -5,3 +5,25 @@ def test_checkout():
     assert checkout("") == 0
     assert checkout("A") == 50
     assert checkout("B") == 30
+    assert checkout("C") == 20
+    assert checkout("D") == 15
+    assert checkout("a") == -1
+    assert checkout("-") == -1
+    assert checkout("ABCa") == -1
+    assert checkout("AxA") == -1
+    assert checkout("ABCD") == 115
+    assert checkout("A") == 50
+    assert checkout("AA") == 100
+    assert checkout("AAA") == 130
+    assert checkout("AAAA") == 180
+    assert checkout("AAAAA") == 230
+    assert checkout("AAAAAA") == 260
+    assert checkout("B") == 30
+    assert checkout("BB") == 45
+    assert checkout("BBB") == 75
+    assert checkout("BBBB") == 90
+    assert checkout("ABCDABCD") == 215
+    assert checkout("BABDDCAC") == 215
+    assert checkout("AAABB") == 175
+    assert checkout("ABCDCBAABCABBAAA") == 505
+
